53be448b8af0a1c24c8821821b7f2dfda6004ad5
syhner <71605633+syhner@users.noreply.github.com>
2026-07-11 00:18:24 +0200

    fix zed extensions file path on linux

--- modules/zed/home.nix
+++ modules/zed/home.nix
@@ -16,7 +16,7 @@
     if kernel == "darwin" then
       "Library/Application Support/Zed/Extensions/index.json"
     else
-      ".config/zed/extensions/index.json"
+      "${config.home.homeDirectory}/.local/share/zed/extensions/index.json"
   }.source =
     config.lib.file.mkOutOfStoreSymlink "${repositoryPath}/modules/zed/extensions.json";
 }
